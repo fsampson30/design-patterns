5d0be6a9ab726322c4ec3f5be6665731be217cec
fsampson30 <fsampson30@gmail.com>
2025-03-05 08:02:56 -0300

    Added OpenApi annotations.

--- src/main/java/com/sampson/design_patterns/controller/ClientController.java
+++ src/main/java/com/sampson/design_patterns/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.sampson.design_patterns.controller;
 
 import com.sampson.design_patterns.model.Client;
 import com.sampson.design_patterns.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
+@Tag(name = "Client Management", description = "Endpoints for managing clients")
 public class ClientController {
 
     private ClienteService clienteService;
@@ -19,6 +22,7 @@ public class ClientController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all clients", description = "Fetch all clients")
     public List<Client> findAll(){
         return clienteService.findALl();
     }
